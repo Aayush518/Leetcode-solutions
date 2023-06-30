4886d82e2587ed355e497a375b54e687a8c47e89
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-06-30 23:35:53 +0545

    update

---
+++ 13.Roman to Integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        total = 0
+        
+        for i in range(len(s)):
+            if i < len(s) - 1 and roman_values[s[i]] < roman_values[s[i+1]]:
+                total -= roman_values[s[i]]
+            else:
+                total += roman_values[s[i]]
+        
+        return total
