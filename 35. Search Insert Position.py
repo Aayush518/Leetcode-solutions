cb9bc51e03e1bd51e97cec6d8296748088b1c13f
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-07-13 19:48:50 +0545

    add

---
+++ 35. Search Insert Position.py
@@ -0,0 +1,28 @@
+# in O(n)
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            for i in range(len(nums)):
+                if target <= nums[i]:
+                    return i
+            return len(nums)
+# in O(log n)
+class Solution:
+    class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
