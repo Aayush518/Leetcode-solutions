e219cecb3e1189f406796dd8302ecf11ff911a7a
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-07-14 22:10:21 +0545

    Create 70. Climbing Stairs.py

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+        prev, curr = 1, 1
+        for i in range(2, n+1):
+            temp = curr
+            curr = prev + curr
+            prev = temp
+        return curr
