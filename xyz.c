20e08b96470d9024364ab87280eef74e7a39aeab
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-07-18 08:47:59 +0545

    UP

---
+++ xyz.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+
+
+
+int main()
+{   
+    char binary_a[9];
+    char binary_b[9];
+
+    printf("Enter the first binary number (up to 8 bits): ");
+    scanf("%8s", binary_a);
+
+    printf("Enter the second binary number (up to 8 bits): ");
+    scanf("%8s", binary_b);
+
+    int a = strtol(binary_a, NULL, 2);
+    int b = strtol(binary_b, NULL, 2);
+        
+    return 0;
+}
